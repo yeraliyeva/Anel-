fedce69525228358795aedae0c321878b586628e
Anel <a_yeraliyeva@kbtu.kz>
2025-01-29 17:52:36 +0500

    lab3

--- lab3/function1.py
+++ lab3/function1.py
@@ -2,22 +2,21 @@
 def gramm(number):
     ounces = number * 28.3495231
     return ounces
-print(gramm(int(input())))
+
 
 # 2)takes temperature in Fahrenheit and converts it to Celsius
 def temp(ff):
     celsius = (5 /9) * (ff-32)
     return celsius
-print(temp(int(input())))
+
 
 # 3)How many rabbits and how many chickens do we have?
 def many(numhead, numleg):
     rabb = (numleg - 2 * numhead) // 2
     chickenn = numhead - rabb
     return rabb, chickenn
-rabb = int(input())
-chickenn = int(input())
-print(many(rabb,chickenn))
+
+
 
 # 4)prime numbers
 def isprime(n):
@@ -29,23 +28,24 @@ def isprime(n):
     return True
 def filter_prime(num): 
     return[ n for n in num if isprime(n)]
-print(filter_prime([2, 3, 4, 5, 6, 7, 8, 9]))
+
 
 #5)Write a function that accepts string from user and print all permutations of that string.
-def perm(s, step=0):
-    if step == len(s):  
-        print("".join(s))
-    else:
-        for i in range(step, len(s)):
-            s_list = list(s)  
-            s_list[step], s_list[i] = s_list[i], s_list[step]  
-            perm(s_list, step + 1)  
-print(perm(input()))
+def new_strings(s):
+    if len(s)==1:
+        return [s]
+    new=[]
+    for i in range(len(s)):
+        for x in new_strings(s[:i]+s[i+1:]):
+            new.append(s[i]+x)
+    return new
+
 
 #6)Write a function that accepts string from user, return a sentence with the words reversed.
 def inverse(word):
-    return " ".join(word.split()[::-1])  
-print(inverse(input()))  
+    txt = word.split()
+    return txt[::-1] 
+ 
 
 #7)Given a list of ints, return True if the array contains a 3 next to a 3 somewhere.
 def has_33(nums):
@@ -55,10 +55,10 @@ def has_33(nums):
             return True
         i += 1
     return False
-numbeers = input()
-nums = numbeers.split()
-nums = [int(num) for num in nums]
-print(has_33(nums))
+numbeers = [45,76,3,3,43,23]
+# nums = numbeers.split()
+# nums = [int(num) for num in nums]
+
 
 #8)Write a function that takes in a list of integers and returns True if it contains 007 in order
 def spy_game(anel):
@@ -68,10 +68,78 @@ def spy_game(anel):
             return True
         i += 1
     return False
-daria = input()
-anel = daria.split()
-anel = [int(num) for num in anel]
-print(spy_game(anel))
+daria = [1,2,34,45,0,0,7,87]
+# anel = daria.split()
+# anel = [int(num) for num in anel]
+
+
+#9) Write a function that computes the volume of a sphere given its radius
+def volume(san):
+    vol = 0
+    vol = (4/3)*math.pi*r**3
+    return vol
+
+
+#10)unique elements 
+def uni(clovo):
+    new_list = []
+    i = 0
+    while i < len(clovo):
+        tf = True
+        j = 0
+        while j < i:
+            if clovo[i]== clovo[j]:
+                tf = False
+            j += 1
+        if tf:
+            new_list.append(clovo[i])
+        i += 1
+    return new_list
+
+
+#11)palindrom
+def pal(p):
+    clov = p[::-1]
+    if clov == p:
+        return True
+    else:
+        return False
+
+
+#12)histogram
+def histogram(daria):
+    for i in daria:
+        print('*'*i)
+
+
+#13) guess number
+import random
+def gus():
+    numm = random.randint(1,20)
+    print("Hello! What is your name?")
+    name = input()
+    print("Well," , name , ", I am thinking of a number between 1 and 20.Take a guess.")
+    chet = 0
+    while True:
+        n = int(input())
+        if n<numm:
+            print("Your guess is too low. Take a guess")
+            chet+=1
+        elif n>numm:
+            print("Your guess is too high.Take a guess")
+            chet+=1
+        elif n==numm:
+            print("Good job, " , name, "! You guessed my number in" , chet , "guesses!")
+            break
+
+
+
+
+
+
+
+
+
 
 
 
